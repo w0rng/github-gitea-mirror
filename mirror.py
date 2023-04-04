6c3d4303bfbac883e86dbf10ed59b7b585edc0a5
w0rng <antonabramov2000@gmail.com>
2023-04-04 10:56:17 +0700

    + sentry

--- mirror.py
+++ mirror.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+import sentry_sdk
+from os import getenv
 
-import gist
-import giststared
-import repoForked
-import repoSource
-import repoStared
+
+
+def main():
+    import gist
+    import giststared
+    import repoForked
+    import repoSource
+    import repoStared
+
+
+if __name__ == "__main__":
+    with open("/cache/cache.json", "w") as f:
+        f.write("{}")
+    if sentry_dsn := getenv("SENTRY_DSN"):
+        sentry_sdk.init(sentry_dsn)
+    main()
